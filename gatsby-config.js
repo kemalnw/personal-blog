15377cc03089ef5321e938da592b295b39bb445a
Abdul Kemal <abdulkemalnw@gmail.com>
2020-07-26 17:29:23 +0700

    feat: change site url

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
     siteImage: `/android-chrome-512x512.png`,
     siteLanguage: `ID`,
     siteDescription: `A personal site by Abdul Kemal`,
-    siteUrl: `https://kemalnw.netlify.app`,
+    siteUrl: `https://kemalnw.id`,
     siteHeadline: `Personal site by Abdul Kemal`,
   },
   plugins: [
